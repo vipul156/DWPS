8ad29ed9dd8465ffbbe53f7d2210b69164f5369b
vipul156 <ak4780321@gmail.com>
2026-02-23 13:28:12 +0530

    more sections added

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { BsQuote } from "react-icons/bs";
 
